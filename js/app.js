5f4bdfcd53e26db8855b0dc044d806ba4321e3e2
Christopher McCorry <cmac@TurboSweetLaptop.local>
2016-05-09 16:53:34 +1000

    Basic logic assembled! Div ID's remapped to squares.

---
+++ js/app.js
@@ -0,0 +1,85 @@
+$(function(){
+
+    var round = 1;
+    var player = "X"
+    var cpu = "O"
+    var clickDOMid
+
+    // Game code
+
+    $('.cell').on('click', function(event){
+        // Catch the user click and return div id
+        var clickID = $(event.target.id);
+
+        clickDOMid = $(event.target)
+        console.log(clickID);
+        console.log(clickDOMid);
+        $(event.text)[0] = "X";
+        console.log(this.id);
+
+    });
+
+
+
+        //
+        //
+        // remaining.text('Rounds Remaining: ' + (numberOfRounds - currentRoundNumber) + 1);
+        //
+        // function getRandomNumber(min, max){
+        //     var rand = Math.floor(Math.random() * (max-min+1) + min);
+        //     console.log(rand);
+        //     return rand;
+        // }
+        //
+        // function placeTarget(){
+        //     target.css({
+        //         left: getRandomNumber(0, gameArea.width() - target.width()),
+        //         top: getRandomNumber(0, gameArea.height() - target.height())
+        //     });
+        //     targetX = target.offset().left - gameArea.offset().left;
+        //     targetY = target.offset().top - gameArea.offset().top;
+        // }
+        //
+        // placeTarget();
+        //
+        // $('h2').on('click', function(event){
+        //     // Scoring exact click
+        //     event.stopPropagation();
+        // });
+        //
+        // $('button').on('click', function(event){
+        //     // Hitting buttons
+        //     event.stopPropagation();
+        // });
+        //
+        // gameArea.on('click', function(event){
+        //     // Scoring a near click;
+        //     if(event.target === target[0]){
+        //         console.log('YOU GOT ME');
+        //         playerScore += exactClick;
+        //
+        //     }else{
+        //         var clickX = event.offsetX;
+        //         var clickY = event.offsetY;
+        //
+        //         var distanceX = Math.abs(targetX - clickX);
+        //         var distanceY = Math.abs(targetY - clickY);
+        //         var distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+        //
+        //         var worstCase = Math.sqrt(gameArea.width() * gameArea.width() + gameArea.height() * gameArea.height());
+        //         var distanceScore = Math.round(Math.pow((1 - distance / worstCase), 2) * exactClick);
+        //         playerScore += distanceScore
+        //     }
+        //     console.log('Your score is: ' + playerScore);
+        //     if(currentRoundNumber < numberOfRounds){
+        //         placeTarget();
+        //         currentRoundNumber++;
+        //     }else {
+        //         console.log('GAME OVER!');
+        //         gameArea.off('click');
+        //     }
+        //     scoreText.text("Score: " + playerScore);
+        // });
+
+
+});
