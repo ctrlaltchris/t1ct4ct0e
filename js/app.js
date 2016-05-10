6c2a9f8b0b929adfd7dc6cf4b70beec37828c54d
Christopher McCorry <cmac@TurboSweetLaptop.local>
2016-05-10 15:49:46 +1000

    p1/p2 now correctly alternating, win conditions still fucked.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    var turn = 1;
+    var turn = 0;
     var round = 1;
     var p1mark = "X";
     var p2mark = "O";
@@ -10,139 +10,63 @@ $(function(){
     var clickScore;
     var playerScore = 0;
     var cpuScore = 0;
-    var currentPlayer = "p1";
-    var nextPlayer;
+    var nextPlayer = "p1";
     var currentPlayerMark;
 
     var wins = [7, 56, 448, 73, 146, 292, 273, 84];
 
-    function playOneRound(){
-        if (currentPlayer = "p1"){
-            currentPlayerMark = p1mark;
-            nextPlayer = "p2"
+    playOneRound();
 
-            $('.cell').on('click', function(event){
-                console.log($(this).text());
-                if ($(this).text() == "") {
+    function playOneRound(){
+
+        $('.cell').on('click', function(event){
+            // Player 1 turn
+            if (nextPlayer === "p1"){
+                currentPlayerMark = p1mark;
+                nextPlayer = "p2"
+
+                if ($(this).text() === "") {
                     $(this).append(currentPlayerMark);
                     p1 += Number(this.id);
-                    console.log(p1);
+                    console.log("p1 score is: " + p1);
+
+                    checkWinner(p1);
+
                 } else {
                     return;
                 }
 
-            });
+            // Player 2 turn
+            } else {
+                currentPlayerMark = p2mark;
+                nextPlayer = "p1"
 
-        }else {
-            currentPlayerMark = p2mark;
-            nextPlayer = "p1"
+                if ($(this).text() === "") {
+                    $(this).append(currentPlayerMark);
+                    p2 += Number(this.id);
+                    console.log("p2 score is: " + p2);
 
-            $('.cell').on('click', function(event){
-                // Catch the user click and return div id
-                var clickElement = this;
-                clickScore = this.id;
-                console.log(this.id);
+                    checkWinner(p2);
 
-                $(clickElement).append(currentPlayerMark);
-                p1 += Number(this.id);
-                console.log(p1);
-                nextPlayer = "p2";
-            });
-        };
+                } else {
+                    return;
+                }
+            }
+
+        });
+
 
     };
 
-    playOneRound();
-
     // Check for a winner!
 
-//     win = function (score) {
-//     for (var i = 0; i < wins.length; i += 1) {
-//         if ((wins[i] & score) === wins[i]) {
-//             return true;
-//         }
-//     }
-//     return false;
-// },
+    function checkWinner(score) {
+        for (var i = 0; i < wins.length; i++) {
+            if (score === wins[i]) {
+                console.log("WINNAR!");
+            }
+        }
+        playOneRound();
+    };
 
 });
-
-
-
-
-
-
-//
-// /*
-//  * Returns whether the given score is a winning score.
-//  */
-//     win = function (score) {
-//         for (var i = 0; i < wins.length; i += 1) {
-//             if ((wins[i] & score) === wins[i]) {
-//                 return true;
-//             }
-//         }
-//         return false;
-//     },
-
-
-
-        //
-        //
-        // remaining.text('Rounds Remaining: ' + (numberOfRounds - currentRoundNumber) + 1);
-        //
-        // function getRandomNumber(min, max){
-        //     var rand = Math.floor(Math.random() * (max-min+1) + min);
-        //     console.log(rand);
-        //     return rand;
-        // }
-        //
-        // function placeTarget(){
-        //     target.css({
-        //         left: getRandomNumber(0, gameArea.width() - target.width()),
-        //         top: getRandomNumber(0, gameArea.height() - target.height())
-        //     });
-        //     targetX = target.offset().left - gameArea.offset().left;
-        //     targetY = target.offset().top - gameArea.offset().top;
-        // }
-        //
-        // placeTarget();
-        //
-        // $('h2').on('click', function(event){
-        //     // Scoring exact click
-        //     event.stopPropagation();
-        // });
-        //
-        // $('button').on('click', function(event){
-        //     // Hitting buttons
-        //     event.stopPropagation();
-        // });
-        //
-        // gameArea.on('click', function(event){
-        //     // Scoring a near click;
-        //     if(event.target === target[0]){
-        //         console.log('YOU GOT ME');
-        //         playerScore += exactClick;
-        //
-        //     }else{
-        //         var clickX = event.offsetX;
-        //         var clickY = event.offsetY;
-        //
-        //         var distanceX = Math.abs(targetX - clickX);
-        //         var distanceY = Math.abs(targetY - clickY);
-        //         var distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-        //
-        //         var worstCase = Math.sqrt(gameArea.width() * gameArea.width() + gameArea.height() * gameArea.height());
-        //         var distanceScore = Math.round(Math.pow((1 - distance / worstCase), 2) * exactClick);
-        //         playerScore += distanceScore
-        //     }
-        //     console.log('Your score is: ' + playerScore);
-        //     if(currentRoundNumber < numberOfRounds){
-        //         placeTarget();
-        //         currentRoundNumber++;
-        //     }else {
-        //         console.log('GAME OVER!');
-        //         gameArea.off('click');
-        //     }
-        //     scoreText.text("Score: " + playerScore);
-        // });
