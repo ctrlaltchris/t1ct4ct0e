a092e317ef3e5e3075e4c016cbd45acdc8a017d5
Christopher McCorry <cmac@TurboSweetLaptop.local>
2016-05-11 15:41:52 +1000

    Draw checker now operational.

--- js/app.js
+++ js/app.js
@@ -8,14 +8,24 @@
     var p2wins = 0;
     var nextPlayer = "p1";
     var currentPlayerMark;
+    var roundCounter = 0
 
     var wins = [7, 56, 448, 73, 146, 292, 273, 84];
+
+    $('#new').on('click', function(event){
+            newGame();
+    });
+
+    $('#reset').on('click', function(event){
+        resetGame();
+    });
 
     playGame();
 
     function playGame(){
 
         $('.cell').on('click', function(event){
+
             // Player 1 turn
             if (nextPlayer === "p1"){
                 currentPlayerMark = p1mark;
@@ -26,7 +36,7 @@
                     p1 += Number(this.id);
                     console.log("p1 score is: " + p1);
 
-                    checkWinner(p1);
+                    checkWinner(p1, nextPlayer);
 
                 } else {
                     return;
@@ -48,16 +58,18 @@
                     return;
                 }
             }
-
         });
-
-
     };
+
+    // CPU player
+    function cpuPlayer() {
+
+    }
 
     // Check for a win
     function checkWinner(score, nextPlayer) {
+        roundCounter++
         for (var i = 0; i < wins.length; i++) {
-            console.log(wins[i] & score);
             if ((wins[i] & score) === wins[i]) {
                 console.log("WINNAR!");
                 if (nextPlayer === "p1"){
@@ -65,23 +77,34 @@
                     $('h1').text("<< p2 WINS! >>");
                     $('#p2score').text("p2 [0] Score: " + p2wins);
                     $('.cell').off('click');
-                }else {
+                }else if (nextPlayer === "p2"){
                     p1wins++;
                     $('h1').text("<< p1 WINS! >>");
                     $('#p1score').text("p1 [X] Score: " + p1wins);
                     $('.cell').off('click');
                 }
-
+            } else if (roundCounter === 9) {
+                $('h1').text("<< IT'S A DRAW! >>");
             }
         }
     };
 
-    function resetBoard() {
+    // Triggered from "New Game" button, resets game board
+    function newGame() {
         $('.cell').empty();
         $('h1').text(" \\\\ Tic Tac Toe! >");
         p1 = 0;
         p2 = 0;
         playGame();
+    }
+
+    // Triggered from "Reset Game" button, resets game board and player scores
+    function resetGame() {
+        newGame();
+        p1score = 0;
+        p2score = 0;
+        $('#p1score').text("p1 [X] Score: 0");
+        $('#p2score').text("p2 [X] Score: 0");
     }
 
 // });
